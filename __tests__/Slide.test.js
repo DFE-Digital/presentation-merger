05dbf24069e63af5634097e9e4804066fbe7ed11
Kyle Welsby <kyle@mekyle.com>
2020-02-18 17:10:36 +0000

    ✨ Extract the slide styles
    Check the slide for slide style ids, and collect the styles for the given slide.

--- __tests__/Slide.test.js
+++ __tests__/Slide.test.js
@@ -1,4 +1,12 @@
+import fs from "fs";
+import path from "path";
+import { get } from "shvl";
+
 import Slide from "../src/Slide";
+
+const fixture = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "__fixtures__/example.json"))
+);
 
 describe("Slide", () => {
   describe(".content", () => {
@@ -23,7 +31,26 @@ describe("Slide", () => {
     });
   });
 
-  describe(".style", () => {
+  describe(".styleIDs", () => {
+    it("returns the ids for the slide", () => {
+      let slideFixture = get(fixture, [
+        "office:document-content",
+        "office:body",
+        "office:presentation",
+        "draw:page",
+        0
+      ]);
+      const slide = new Slide(slideFixture, fixture);
+      let actual = slide.styleIDs;
+
+      expect(actual).toContain("a776");
+      expect(actual).toContain("a779");
+      expect(actual).toContain("a786");
+      expect(actual).toContain("a818");
+    });
+  });
+
+  describe(".styles", () => {
     it("copies without reference the originalContent", () => {
       let originalContent = {
         greet: "hello world",
@@ -31,7 +58,7 @@ describe("Slide", () => {
       };
 
       const slide = new Slide(originalContent);
-      let actual = slide.style;
+      let actual = slide.styles;
 
       expect(actual).toEqual(originalContent);
 
@@ -42,6 +69,24 @@ describe("Slide", () => {
 
       expect(actual).toHaveProperty("deep.thing", "changed");
       expect(originalContent).toHaveProperty("deep.thing", "original");
+    });
+
+    it("includes the styles for the given slide", () => {
+      let slideFixture = get(fixture, [
+        "office:document-content",
+        "office:body",
+        "office:presentation",
+        "draw:page",
+        0
+      ]);
+      const slide = new Slide(slideFixture, fixture);
+      let actual = slide.styles;
+
+      expect(actual).toContainEqual(
+        expect.objectContaining({
+          "style:name": "a776"
+        })
+      );
     });
   });
 });
--- src/Slide.js
+++ src/Slide.js
@@ -1,14 +1,37 @@
+import { get } from "shvl";
+
 export default class Slide {
-  constructor(content, style) {
+  constructor(content, presentation) {
     this.originalContent = content;
-    this.originalStyle = style;
+    this.originalPresentation = presentation;
   }
 
   get content() {
     return JSON.parse(JSON.stringify(this.originalContent));
   }
 
-  get style() {
-    return JSON.parse(JSON.stringify(this.originalContent));
+  get styleIDs() {
+    let ids = [];
+    ids.push(this.content["draw:style-name"]);
+    ["draw:frame", "draw:custom-shape", "draw:connector"].forEach(key => {
+      if (this.content[key]) {
+        this.content[key].forEach(item => {
+          ids.push(item["draw:style-name"]);
+        });
+      }
+    });
+
+    return ids;
+  }
+
+  get styles() {
+    const documentStyles = get(this.originalPresentation, [
+      "office:document-content",
+      "office:automatic-styles",
+      "style:style"
+    ]);
+    return documentStyles.filter(item => {
+      return this.styleIDs.includes(item["style:name"]);
+    });
   }
 }
