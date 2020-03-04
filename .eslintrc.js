c3e3d9941967fa5b307fa76ddbe632c24604fa1b
Kyle Welsby <kyle@mekyle.com>
2020-03-04 10:58:08 +0000

    🚨 ESLint allow console.error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,17 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true
+    node: true,
   },
-  parser: "babel-eslint",
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
-    "prettier/prettier": "off"
-  }
+    'prettier/prettier': 'off',
+    'no-console': [
+      'error',
+      {
+        allow: ['error'],
+      },
+    ],
+  },
 };
